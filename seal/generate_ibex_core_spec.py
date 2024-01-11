fe997d56673502e04ecc750e897c1f9f9f8c55bf
salties <yanyansmajesty@outlook.com>
2024-01-11 15:55:36 +0100

    Added components.

--- seal/generate_ibex_core_spec.py
+++ seal/generate_ibex_core_spec.py
@@ -12,5 +12,24 @@ ibex_core.NewComponent("SimTime", "UINT32")
 # 32 general register files.
 ibex_core.NewComponent("Reg", "UINT32", 32)
 
+# Execution instruction.
+ibex_core.NewComponent("ExeInst", "OCTET")
+
+# Execution operands.
+ibex_core.NewComponent("ExeOpA", "UINT32")
+ibex_core.NewComponent("ExeOpB", "UINT32")
+
+# Execution result.
+ibex_core.NewComponent("ExeResult", "UINT32")
+
+# Instruction memory read bus.
+ibex_core.NewComponent("MemInstRdBus", "UINT32")
+
+# Data memory read bus.
+ibex_core.NewComponent("MemDataRdBus", "UINT32")
+
+# Data memory read bus.
+ibex_core.NewComponent("MemDataWrBus", "UINT32")
+
 # Save the Core Specification.
 ibex_core.Save("./ibex.scs")
